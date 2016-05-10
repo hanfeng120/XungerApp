9e8876b8ec89819bb10063de20e911af4613506a
zhaoxunyi <zhaoxunyi@jiemian.com>
2016-05-10 11:11:08 +0800

    AppUtil

---
+++ app/src/main/java/club/xunger/andapp/gesture/utils/Constants.java
@@ -0,0 +1,13 @@
+package club.xunger.andapp.gesture.utils;
+
+public class Constants {
+
+    /**
+     * 手势密码点的状态
+     */
+    public static final int POINT_STATE_NORMAL = 0;
+
+    public static final int POINT_STATE_SELECTED = 1;
+
+    public static final int POINT_STATE_WRONG = 2;
+}
